ba9b1dbc8e7511c26b307b238bfe4c41c22354a3
Michael Yan <rain@rainboyan.com>
2022-11-03 21:30:41 +0800

    Update to Grails 2022.0.0 snapshot

--- grails-app/init/org/grails/demo/Application.groovy
+++ grails-app/init/org/grails/demo/Application.groovy
@@ -1,16 +1,15 @@
 package org.grails.demo
 
-import grails.boot.GrailsApp
-import grails.boot.config.GrailsAutoConfiguration
+import grails.boot.Grails
 import grails.util.Environment
 
 import groovy.transform.CompileStatic
 
 @CompileStatic
-class Application extends GrailsAutoConfiguration {
+class Application {
 
     static void main(String[] args) {
-        GrailsApp.run(Application, args)
+        Grails.run(Application, args)
 
         println "Reloading enabled? " + Environment.isReloadingAgentEnabled()
     }
