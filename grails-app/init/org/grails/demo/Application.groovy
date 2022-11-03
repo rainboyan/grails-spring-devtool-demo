c74cd4398fa39d82e4817cb43cb21ba4548e82b9
Michael Yan <rain@rainboyan.com>
2022-11-03 18:52:56 +0800

    Grails Environment.isReloadingAgentEnabled
    Update spring.devtools.restart.enabled=true

--- grails-app/init/org/grails/demo/Application.groovy
+++ grails-app/init/org/grails/demo/Application.groovy
@@ -2,12 +2,17 @@ package org.grails.demo
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import grails.util.Environment
 
 import groovy.transform.CompileStatic
 
 @CompileStatic
 class Application extends GrailsAutoConfiguration {
+
     static void main(String[] args) {
         GrailsApp.run(Application, args)
+
+        println "Reloading enabled? " + Environment.isReloadingAgentEnabled()
     }
+
 }
